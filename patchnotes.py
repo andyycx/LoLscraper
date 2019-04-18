942fdb10bfda48777ed3294c6111bc96b542a6a5
Andy Ying <42513913+andyycx@users.noreply.github.com>
2019-04-18 19:42:26 +0100

    Add files via upload

---
+++ patchnotes.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import functools
+
+f = open("lolpatches.txt", "w+")
+
+r = requests.get("https://euw.leagueoflegends.com/en/news/game-updates/patch")
+soup = BeautifulSoup(r.text, "html.parser")
+last_patch = soup.find("div", "views-row views-row-1 views-row-odd views-row-first").text.strip()
+
+for replacement in (("Notes", "Notes:\n"), ("notes", "Notes:\n")):
+    last_patch = last_patch.replace(*replacement)
+f.write(last_patch + "\n")
+
+link = "https://euw.leagueoflegends.com" + soup.find("div", "views-row views-row-1 views-row-odd views-row-first").find("a").get("href")
+f.write(link)
+
+r = requests.get(link)
+soup = BeautifulSoup(r.text, "html.parser")
+contents = soup.find("div", id="patch-notes-container").text.strip()
+
+f.write(contents.replace("\u21d2", "TO"))
+
+##for box in patch_boxes:
+##    patches.append(box.text.strip())
+##
+##for patch in patches:
+##    for r in (("Notes", "Notes: "), ("notes", "Notes: ")):
+##        patch = patch.replace(*r)
+##    f.write(patch + "\n" + "\n")
+##
+f.close()
